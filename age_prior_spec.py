97a677847e29d76f4f7d1471d3eac2952acf6d6d
Edderic Ugaddan <edderic@gmail.com>
2019-04-08 08:51:31 -0400

    test age_prior

---
+++ age_prior_spec.py
@@ -0,0 +1,37 @@
+""" Test for generating sensible priors for age for a certain year. """
+from mamba import description, context, it, before
+from lib.prior_generation import age_prior
+import pandas as pd
+from math import isclose
+
+with description('age_prior') as self:
+    with before.each:
+        self.args = {}
+        self.years = [str(i) for i in range(1980, 1986)]
+
+    with context('when year is 1983'):
+        with before.each:
+            self.args['year'] = 1983
+
+        with context('and whale was first seen in 1984'):
+            with before.each:
+                self.args['row'] = pd.Series([0, 0, 0, 0, 1, 0], index=self.years)
+
+            with context('and whale was observed as a calf'):
+                with before.each:
+                    self.args['known'] = True
+
+                with it('should give a uniform prior between -1 and 0 '):
+                    self.rv = age_prior(**self.args)
+                    assert self.rv.mean() == -0.5
+                    assert isclose(self.rv.var(), 0.083, abs_tol=0.01)
+
+            with context('and whale was NOT observed as a calf'):
+                with before.each:
+                    self.args['known'] = False
+
+                with it('should give a uniform prior between 0 and 80 '):
+                    self.rv = age_prior(**self.args)
+                    import pdb; pdb.set_trace()
+                    assert self.rv.mean() == 40
+                    # assert isclose(self.rv.var(), 0.083, abs_tol=0.01)
