1dbd197ca2d9eed11aac6d2d5ce7b789a9cfd28b
Edderic Ugaddan <edderic@gmail.com>
2019-04-08 10:08:53 -0400

    use HadBirthsBeforeQuadratic

--- had_births_before_spec.py
+++ had_births_before_spec.py
@@ -1,9 +1,9 @@
-""" Tests methods on HadBirthsBefore """
+""" Tests methods on HadBirthsBeforeQuadratic """
 from mamba import description, context, it, before
-from lib.sampler import HadBirthsBefore
+from lib.sampler import HadBirthsBeforeQuadratic
 from spec.helpers import set_to_value_except
 
-with description('HadBirthsBefore') as self:
+with description('HadBirthsBeforeQuadratic') as self:
     with before.each:
         self.args = {}
         self.arguments = [
@@ -12,8 +12,8 @@ with description('HadBirthsBefore') as self:
             'prior_constant',
             'prior_age',
             'yspb',
-            'prior_yspb',
-            'prior_yspb_squared'
+            'prior_peak_yspb',
+            'prior_width'
         ]
 
     with description('#proba_give_birth'):
@@ -31,7 +31,7 @@ with description('HadBirthsBefore') as self:
             with context('but whale is NOT reproductively active'):
                 with before.each:
                     self.args['repr_active'] = 0
-                    self.subject = HadBirthsBefore(**self.args).proba_give_birth()
+                    self.subject = HadBirthsBeforeQuadratic(**self.args).proba_give_birth()
 
                 with it('should return 0'):
                     assert self.subject == 0
@@ -39,10 +39,10 @@ with description('HadBirthsBefore') as self:
             with context('and whale reproductively active'):
                 with before.each:
                     self.args['repr_active'] = 1
-                    self.subject = HadBirthsBefore(**self.args).proba_give_birth()
+                    self.subject = HadBirthsBeforeQuadratic(**self.args).proba_give_birth()
 
                 with it('should return the weighted sums run through logistic function'):
-                    assert self.subject > 0.99
+                    assert self.subject > 0.95 and self.subject < 0.96
 
         with context('when whale gave birth last year'):
             with before.each:
@@ -52,7 +52,7 @@ with description('HadBirthsBefore') as self:
                     value=1,
                     _except=[]
                 )
-                self.subject = HadBirthsBefore(**self.args).proba_give_birth()
+                self.subject = HadBirthsBeforeQuadratic(**self.args).proba_give_birth()
 
             with it('should return 0'):
                 assert self.subject == 0
